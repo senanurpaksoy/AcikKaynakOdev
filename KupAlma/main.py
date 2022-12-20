8e9c0fcf0711f1e023e4868f9b55f5918e2d2ea5
HakanKayli <97335482+HakanKayli@users.noreply.github.com>
2022-12-20 10:14:42 +0300

    armstrong sayı fonksiyonu eklendi

--- KupAlma/main.py
+++ KupAlma/main.py
@@ -9,6 +9,21 @@ class Kup(Resource):
     def get(self,sayi):
         return {'Kupu': sayi*sayi*sayi},200
 
+class ArmstrongSayi(Resource):
+    def get(self,sayi):
+        uzunluk = len(sayi)
+        toplam = 0
+        
+        for i in range(uzunluk):
+            toplam = toplam + int(sayi[i]) ** uzunluk
+        
+        if (toplam == int(sayi)):
+            return {'Girdiginiz sayi bir amstrong sayisidir'},200
+        else:
+            return {'Girdiğiniz sayi bir amstrong sayisi değildir'},200
+        
+
+api.add_resource(ArmstrongSayi, '/armstrong/<int:sayi>'}        
 api.add_resource(Kup, '/kup/<int:sayi>')
 app.run(host="0.0.0.0", port=5000)
 app.run()
